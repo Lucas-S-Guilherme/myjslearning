baf584fe8c3340dc34a1a660c365d610aa487eb6
Lucas Guilherme <lucassguilherme159@gmail.com>
2024-05-30 16:56:14 -0400

    iniciando 1º capítulo

---
+++ Cap1/Variaveis.js
@@ -0,0 +1,12 @@
+// JS não é fortemente tipado.
+var num = 1;
+num = 3;
+var price = 1.5;
+var myName = 'Lucas';
+var trueValue = true;
+var nullVar = null; // nulo, sem valor
+var und; //indefinido, nenhum valor
+
+//Imprimir as variáveis
+
+console.log('num: ' + num);
