246ea8c7ef9cf18e4db6042aa5ca97120d443485
Lucas Guilherme <lucassguilherme159@gmail.com>
2024-06-03 21:54:39 -0400

    no caminho para o fim

--- Cap1/Variaveis.js
+++ Cap1/Variaveis.js
@@ -1,6 +1,6 @@
 // JS não é fortemente tipado.
 var num = 1;
-num = 3;
+num = 35;
 var price = 1.5;
 var myName = 'Lucas';
 var trueValue = true;
@@ -10,3 +10,28 @@ var und; //indefinido, nenhum valor
 //Imprimir as variáveis
 
 console.log('num: ' + num);
+// O método console.log também aceita mais do que apenas argumentos. É possível utilizar vírgula (,) - o que permite adicionar uma descrição e visualizar o conteúdo da variável caso ela seja um objeto.
+console.log('num: ', num); // executei direto no debug console
+
+//Escopo das Variáveis - Global ou local
+
+var myVariable = 'global';
+myOtherVariable = 'global';
+
+function myFunction (){
+    var myVariable = 'local';
+    return myVariable;
+}
+
+function myOtherFunction(){
+    myOtherVariable = 'local';
+    return myOtherVariable;
+}
+
+console.log(myVariable);
+console.log(myFunction());
+console.log(myOtherVariable);
+console.log(myOtherFunction());
+console.log(myOtherVariable);
+
+//operadores
